183b7ec4bb9a0723939f7422fa598cf498be39ca
Лев Краснов <levkrasnov@MacBook-Pro-Lev-2.local>
2026-03-20 20:17:51 +0300

    add link to Scientific Data paper

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ MixtureSolDB contains 175166 experimental solubility values within a temperature
 
 Visit our demo <https://mixturesoldb.streamlit.app/>.
 
+Malikov, D., Krasnov, L., Kiseleva, M. et al. **Dataset of solubility values for organic compounds in binary mixtures of solvents at various temperatures**. Sci Data (2026). https://doi.org/10.1038/s41597-026-07047-z
+
 Download MixtureSolDB: [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.18234735.svg)](https://doi.org/10.5281/zenodo.18234735).
--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ st.set_page_config(page_title='MixtureSolDB', layout="wide")
 df = pd.read_csv('MixtureSolDB.csv')
 df['PubChem_CID'] = df['PubChem_CID'].astype('Int64')
 compound_names = sorted(df['Compound_Name'].unique().tolist())
-fda_compound_names = sorted(df[df['FDA_Approved'] == 'Yes']['Compound_Name'].unique().tolist())
+fda_compound_names = sorted(df[df['FDA_Approved'] == True]['Compound_Name'].unique().tolist())
 
 df_smiles = pd.DataFrame({'SMILES_Solute': list(df['SMILES_Solute'].unique())})
 df_smiles['mol'] = df_smiles['SMILES_Solute'].apply(Chem.MolFromSmiles)
@@ -118,6 +118,8 @@ col1intro.markdown(f"""
 # MixtureSolDB
 
 Download MixtureSolDB: [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.18234735.svg)](https://doi.org/10.5281/zenodo.18234735)
+
+If you are using our database or the App please cite: Malikov, D., Krasnov, L., Kiseleva, M. et al. **Dataset of solubility values for organic compounds in binary mixtures of solvents at various temperatures**. Sci Data (2026). https://doi.org/10.1038/s41597-026-07047-z
                    
 """)
 
